b7231577f81f31a7b2cca90ceaa26ad97957443c
SERGIO GARCÍA VICENTE <sergio.gvicente@edu.upct.es>
2021-07-25 17:39:53 +0200

    Updates

---
+++ precache-manifest.f8eeac61fcdff681741a8a1008d3cc48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438f1fa31d407ad748f8e0d76126a640",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0db0832be424fe639506",
+    "url": "./static/css/2.45c371c4.chunk.css"
+  },
+  {
+    "revision": "ba02ab765c2a59f940d0",
+    "url": "./static/css/main.cce32172.chunk.css"
+  },
+  {
+    "revision": "0db0832be424fe639506",
+    "url": "./static/js/2.b0d2a3b7.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.b0d2a3b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba02ab765c2a59f940d0",
+    "url": "./static/js/main.c1b4718b.chunk.js"
+  },
+  {
+    "revision": "806f74c2a44e8c38a246",
+    "url": "./static/js/runtime-main.6d8fe43c.js"
+  }
+]);
